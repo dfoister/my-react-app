fd5a5e2c2f267e026c7f4e3ef895be806a40d233
dev-foister-cko <112949066+dev-foister-cko@users.noreply.github.com>
2023-04-06 22:21:18 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is a WIP :)
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Real Website
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
